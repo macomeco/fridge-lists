2a1e699ac852594e9e5c27102e802bef2bbd2214
Hyuk <Hyuk@gmail.com>
2020-04-06 08:49:45 +0000

    user+

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:show]
-  before_action :correct_user, only: [:destroy, :edit, :show, :update,]
+  #before_action :correct_user, only: [:destroy, :edit, :show, :update,]
 
   def show
     @user = User.find(params[:id])
@@ -28,12 +28,12 @@ class UsersController < ApplicationController
   end
 
   def update
-      if @user.update(user_params)
-      flash[:success] = 'タグを編集しました'
-      redirect_back(fallback_location: root_path)
+      if current_user.update(user_params)
+        flash[:success] = 'タグを編集しました'
+        redirect_back(fallback_location: root_path)
       else
-          flash.now[:danger] = 'タグは編集されませんでした'
-      redirect_back(fallback_location: root_path)
+        flash.now[:danger] = 'タグは編集されませんでした'
+        redirect_back(fallback_location: root_path)
       end
   end
 
@@ -42,7 +42,6 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
 
 end
 
